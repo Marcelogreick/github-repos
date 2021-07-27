e29a6e5fccf93b3ecc19018dd8fd8f8cfe22b916
Greick <marcelo.greick@deway.com.br>
2021-07-27 01:44:02 -0300

    integration complete

---
+++ README.md
@@ -0,0 +1,33 @@
+## Projeto
+
+Teste desenvolvido para a [Uol] <br />
+
+## Tecnologias utilizadas 🚀
+
+<ul>
+    <li><a href="https://reactjs.org/" target="_blank">React.js</a></li>
+    <li><a href="https://nextjs.org/" target="_blank">Next.Js</a></li>
+    <li><a href="https://sass-lang.com/" target="_blank">Sass</a></li>
+</ul>
+
+
+<br />
+<img src="./public/repos.png" alt="print" height=400 width=800/>
+
+
+## Executar a aplicação
+
+Em caso de download do projeto você precisará baixar as dependências:
+
+**Necessário:** Yarn , utilize Yarn install
+
+<ul>
+ <li>Para iniciar o projeto, siga os passos abaixo:</li>
+</ul>
+
+```
+1 - Primeiro abra no terminal a pasta do backend e execute yarn dev
+2 - Dentro desta aplicação frontend execute yarn dev
+
+O .ENV FOI ENVIADO NO GIT PARA FACILITAR A EXECUÇÃO
+```
--- src/pages/init/index.js
+++ src/pages/init/index.js
@@ -1,7 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import { Container, Row } from "reactstrap";
-import { getSession, session, useSession } from "next-auth/client"
+import { Button, Container, Row } from "reactstrap";
+import { getSession } from "next-auth/client"
 import { ButtonLogged } from "../../components/ButtonLogged";
 import { useEffect, useState } from "react";
 import api from '../../services/api';
@@ -9,30 +8,100 @@ import api from '../../services/api';
 import styles from './styles.module.scss';
 
 export default function Init({props}) {
-  session = useSession()
+  const [inputUser, setInputUser] = useState('');
+  const [user, setUser] = useState('');
+  const [repository, setRepository] = useState([]);
+  const [starred, setStarred] = useState([]);
 
-  // const [inputUser, setInputUser] = useState('');
-  // const [user, setUser] = useState('');
+  function handleUser(event) {
+    event.preventDefault();
 
-  // useEffect((
-  //   api.get(`/users/${inputUser}`).then((response) => {
-  //     setUser(response.data)
-  //   })
-  // ), [inputUser, user])
+    api.get(`/users/${inputUser}`).then((response) => {
+      setUser(response.data)
+    }).catch((error) => {
+      alert('O usuário não existe')
+    })
+
+    setStarred([])
+    setRepository([])
+  }
+
+  async function handleRepository(event) {
+    event.preventDefault();
+
+    const response = await api.get(`/users/${user.login}/repos`)
+    setRepository(response.data)
+    setStarred([])
+  }
+
+  async function handleStared(event) {
+    event.preventDefault();
+
+    const response = await api.get(`/users/${user.login}/starred`)
+
+    setStarred(response.data)
+    setRepository([])
+  }
+
+  useEffect(() => {
+    console.log(repository)
+  }, [repository])
 
   return(
     <Container>
-      <Row className={styles.header}>
+      <Row className={styles.content}>
         <ButtonLogged />
-        <form>
+        <form onSubmit={handleUser}>
           <input
-          // value={inputUser}
-          // onChange={(e) => setInputUser(e.target.value)}
+          value={inputUser}
+          onChange={(e) => setInputUser(e.target.value)}
           placeholder="Digite o nome do usuário"
           />
 
           <button type="submit">Pesquisar</button>
         </form>
+        
+        {!!user ? (
+          <>
+            <h1>Usuário: {user.login}</h1> 
+            <div>
+              <Button size="lg" onClick={handleRepository}>Repositórios</Button>
+              <Button size="lg" onClick={handleStared}>Mais visitados</Button>
+            </div>
+          </>
+        ): ''}
+      </Row>
+
+      <Row>
+        {repository.length !== 0 ? (
+          <Row className={styles.repository}>
+            {repository.map(repository => (
+              <div key={repository.id}>
+                <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+                  <div>
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
+                  </div>
+              </div>
+            ))}
+          </Row>
+          ) : ''}
+      </Row>
+
+      <Row>
+        {starred.length !== 0 ? (
+          <Row className={styles.repository}>
+            {starred.map(repository => (
+              <div key={repository.id}>
+                <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+                  <div>
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
+                  </div>
+              </div>
+            ))}
+          </Row>
+          ) : ''}
       </Row>
     </Container>
   );
